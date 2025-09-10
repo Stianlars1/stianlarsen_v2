04927ae3450a062487a46602309711c9ecdd2827
git <stian.larsen@sparebank1.no>
2025-09-10 19:57:45 +0200

    Updated about secton

--- src/features/about/about.tsx
+++ src/features/about/about.tsx
@@ -3,6 +3,10 @@ import styles from "./css/about.module.css";
 
 const TASKBUDDY_URL = "https://taskbuddy.dev/";
 const TINIFY_URL = "https://tinify.dev/";
+const SONIO_URL = "https://sonio.fm/";
+const RAMPKIT_URL = "https://rampkit.app/";
+const CSS_VARIABLES_ASSISTANT_URL = "https://www.css-variables-assistant.dev/";
+
 export const About = () => {
   return (
     <section id={"about"} className={styles.section}>
@@ -16,9 +20,18 @@ export const About = () => {
           the intersection of design and backend engineering.
         </p>
         <p>
-          My journey into technology began in my father’s hobby room, where
+          My journey into technology began in my father&apos;s hobby room, where
           tinkering with computers sparked a lifelong fascination. From building
-          a SoundCloud clone to launching{" "}
+          what started as a SoundCloud clone and evolved into{" "}
+          <Link
+            aria-label="Link to Sonio, Stian Larsen's advanced music streaming platform"
+            href={SONIO_URL}
+            target="_blank"
+            rel="noreferrer"
+          >
+            Sonio.fm
+          </Link>{" "}
+          , to launching productivity tools like{" "}
           <Link
             aria-label="Link to Stian Larsen's own task management app located at TaskBuddy.dev"
             href={TASKBUDDY_URL}
@@ -36,8 +49,26 @@ export const About = () => {
           >
             Tinify.dev
           </Link>{" "}
+          , to creating developer tools like{" "}
+          <Link
+            aria-label="Link to Rampkit, Stian Larsen's color palette generator for developers"
+            href={RAMPKIT_URL}
+            target="_blank"
+            rel="noreferrer"
+          >
+            Rampkit
+          </Link>{" "}
+          and a{" "}
+          <Link
+            aria-label="Link to CSS Variables Assistant, Stian Larsen's JetBrains IDE plugin"
+            href={CSS_VARIABLES_ASSISTANT_URL}
+            target="_blank"
+            rel="noreferrer"
+          >
+            JetBrains IDE plugin
+          </Link>{" "}
           , I enjoy creating tools that simplify and enhance everyday tasks.
-          When I’m not coding, I cherish time with my girlfriend and our
+          When I&apos;m not coding, I cherish time with my girlfriend and our
           18-month-old son, binge-watching movies or brainstorming the{" "}
           <Link
             aria-label="Link to nextjs org site, the provider of the React framework"
@@ -49,7 +80,6 @@ export const About = () => {
           </Link>{" "}
           web application.
         </p>
-
         <p>
           Driven by ambition, creativity, and a love for problem-solving, I’m
           dedicated to building user-friendly, high-performance applications
